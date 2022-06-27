d7af3b2535bdd147da0ca9fea46e292ef9fecea9
robinarehen <robinarehen@gmail.com>
2022-06-27 13:10:39 +0200

    se crea el test de servicio completo y operando correctamente

---
+++ src/test/java/com/example/demo/crud/app/service/DireccionServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.demo.crud.app.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class DireccionServiceTest {
+
+	private DireccionService direccionService;
+
+	@BeforeEach
+	public void init() {
+
+	}
+}
